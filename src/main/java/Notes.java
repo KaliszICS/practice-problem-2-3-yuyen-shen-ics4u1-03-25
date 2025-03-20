9c95769ce089130cb8baaa3a91042132240f63c5
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-03-20 18:41:08 +0000

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -2,56 +2,58 @@
 class Notes {
 	public static void main(String[] args) {
 
-		//Bubble sort
+		// Bubble sort
 
-		int numArray[] = {54, 5, 8, 2, 54, 78, 1, 0, 54};
+		int numArray[] = { 54, 5, 8, 2, 54, 78, 1, 0, 54 };
 		boolean swapped = true;
 
-		for (int i = 0; i < numArray.length - 1 && swapped; i++ ) {
+
+		for (int j = 0; j < numArray.length - 1 && swapped; j++) {
 			swapped = false;
-			for (int j = 0; j < numArray.length - 1 - i; j++)  {
-				if (numArray[j] > numArray[j+1]) {
-					//swap
-					int temp = numArray[j];
-					numArray[j] = numArray[j + 1];
-					numArray[j + 1] = temp;
+			for (int i = 0; i < numArray.length - 1 - j; i++) {
+				if (numArray[i] > numArray[i + 1]) {
+					// swap
+					int temp = numArray[i];
+					numArray[i] = numArray[i + 1];
+					numArray[i + 1] = temp;
 					swapped = true;
 				}
 			}
+			// if (!swapped) {
+			// 	j = numArray.length; //end our loops
+			// }
 		}
 
-	
 		for (int i = 0; i < numArray.length; i++) {
 			System.out.print(numArray[i] + " ");
 		}
 
+		// for (int i = 1; i < numArray.length; i++) {//how many elements at the end of
+		// the array the code ignores
+		// boolean sorted = true;
+		// for (int j = 0; j < numArray.length - i; j++) { //loops the unsorted section
+		// of the array
 
-		
-		
-		// for (int i = 1; i < numArray.length; i++) {//how many elements at the end of the array the code ignores
-		// 	boolean sorted = true;
-		// 	for (int j = 0; j < numArray.length - i; j++) { //loops the unsorted section of the array
-				
-		// 		//When we swap
-		// 		if (numArray[j] > numArray[j+1]) {
-		// 			//Swap the two elements
-		// 			int temp;
-		// 			temp = numArray[j];
-		// 			numArray[j] = numArray[j+1];
-		// 			numArray[j+1] = temp;
-		// 			//change sorted to false becuase we swapped
-		// 			sorted = false;
-		// 		}
-		// 	} 
-		// 	//optimization to stop when it is sorted
-		// 	if (sorted) {
-		// 		break;
-		// 	}
+		// //When we swap
+		// if (numArray[j] > numArray[j+1]) {
+		// //Swap the two elements
+		// int temp;
+		// temp = numArray[j];
+		// numArray[j] = numArray[j+1];
+		// numArray[j+1] = temp;
+		// //change sorted to false becuase we swapped
+		// sorted = false;
+		// }
+		// }
+		// //optimization to stop when it is sorted
+		// if (sorted) {
+		// break;
+		// }
 		// }
 
 		// for (int i = 0; i < numArray.length; i++) {
-		// 	System.out.print(numArray[i] + " ");
+		// System.out.print(numArray[i] + " ");
 		// }
-		
+
 	}
 }
