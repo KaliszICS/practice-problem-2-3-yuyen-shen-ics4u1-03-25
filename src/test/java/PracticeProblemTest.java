3f0f9712625bee4603b2616badd07426a28a3dea
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-03-19 22:08:47 -0400

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,49 +1,72 @@
+import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.io.*;
-
 public class PracticeProblemTest {
+@Test
+@DisplayName("")
+void BubbleSortTest3() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {String[].class};
+        Method method = testClass.getDeclaredMethod("bubbleSortString", cArg);
+  // Enter code here
+  String[] unsorted = {"1"};
+	String[] sorted = {"1"};
+  	(String[])method.invoke(null, unsorted);
+	assertArrayEquals(sorted, unsorted);
+}
 
-   @Test
-   public void testOutput()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
 
-     // action
-     PracticeProblem.q1();
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void BubbleSortTest2() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {String[].class};
+        Method method = testClass.getDeclaredMethod("bubbleSortString", cArg);
+  // Enter code here
+  String[] unsorted = {};
+	String[] sorted = {};
+  	(String[])method.invoke(null, unsorted);
+	assertArrayEquals(sorted, unsorted);
+}
 
-     // assertion
-     assertEquals("There once was a man from St. Ives.\n", bos.toString());
 
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void bubbleSortTest1() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {String[].class};
+        Method method = testClass.getDeclaredMethod("bubbleSortString", cArg);
+  // Enter code here
+  String[] unsorted = {"a", "b", "D", "c", "help", "me", "you", "fool"};
+	String[] sorted = {"a", "b", "c", "D", "fool", "help", "me", "you"};
+  	(String[])method.invoke(null, unsorted);
+	assertArrayEquals(sorted, unsorted);
+}
 
-   @Test
-   public void testInputandOutput()
-   {
-      String data = "Users Input";
-      System.setIn(new ByteArrayInputStream(data.getBytes()));
-      
-      PrintStream originalOut = System.out;
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(bos));
 
-      // action
-      PracticeProblem.q1();
-
-      // assertion
-      assertEquals("There once was a man from St. Ives.\n", bos.toString());
-
-      // undo the binding in System
-      System.setOut(originalOut);
-   }
-
-   @Test
-   public void testQ3()
-   {
-     
-   }
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
 }
