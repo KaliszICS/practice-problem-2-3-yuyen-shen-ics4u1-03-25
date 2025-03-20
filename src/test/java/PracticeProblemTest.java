ad348954e1fe6f825cd75a6e64e73d35dd9611fe
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-03-20 13:14:56 +0000

    update tests

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,19 +1,19 @@
 import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
-import java.io.*;
+
 public class PracticeProblemTest {
 @Test
 @DisplayName("")
 void BubbleSortTest3() {
     Class<?> testClass = PracticeProblem.class;
     try {
-        Class[] cArg = {String[].class};
+        Class<?>[] cArg = {String[].class};
         Method method = testClass.getDeclaredMethod("bubbleSortString", cArg);
   // Enter code here
   String[] unsorted = {"1"};
 	String[] sorted = {"1"};
-  	(String[])method.invoke(null, unsorted);
+  	method.invoke(null, new Object[]{unsorted});
 	assertArrayEquals(sorted, unsorted);
 }
 
@@ -30,12 +30,12 @@ void BubbleSortTest3() {
 void BubbleSortTest2() {
     Class<?> testClass = PracticeProblem.class;
     try {
-        Class[] cArg = {String[].class};
+        Class<?>[] cArg = {String[].class};
         Method method = testClass.getDeclaredMethod("bubbleSortString", cArg);
   // Enter code here
   String[] unsorted = {};
 	String[] sorted = {};
-  	(String[])method.invoke(null, unsorted);
+  	method.invoke(null, new Object[]{unsorted});
 	assertArrayEquals(sorted, unsorted);
 }
 
@@ -52,12 +52,12 @@ void BubbleSortTest2() {
 void bubbleSortTest1() {
     Class<?> testClass = PracticeProblem.class;
     try {
-        Class[] cArg = {String[].class};
+        Class<?>[] cArg = {String[].class};
         Method method = testClass.getDeclaredMethod("bubbleSortString", cArg);
   // Enter code here
   String[] unsorted = {"a", "b", "D", "c", "help", "me", "you", "fool"};
 	String[] sorted = {"a", "b", "c", "D", "fool", "help", "me", "you"};
-  	(String[])method.invoke(null, unsorted);
+  	method.invoke(null, new Object[]{unsorted});
 	assertArrayEquals(sorted, unsorted);
 }
 
